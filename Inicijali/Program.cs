bd31b95420cbab0b77f26df9d2481837616ccf94
prof <prof@KAT42>
2021-10-08 11:40:59 +0200

    Add project files.

---
+++ Inicijali/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inicijali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ime: ");
+            var ime = Console.ReadLine();
+            Console.Write("Prezime: ");
+            var prezime = Console.ReadLine();
+            Console.WriteLine("Vaši inicijali su: {0}.{1}.",
+                ime[0], prezime.Substring(0, 1));
+        }
+    }
+}
